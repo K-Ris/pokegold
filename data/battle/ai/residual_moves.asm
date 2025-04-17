7a9081841a70faece9e2b6d7508ecd24366061f5
Christina Pagger <christina.pagger@uni-graz.at>
2025-04-17 10:10:13 +0200

    battle mechanics overhauls

--- data/battle/ai/residual_moves.asm
+++ data/battle/ai/residual_moves.asm
@@ -1,16 +1,20 @@
 ; AI_CAUTIOUS discourages these moves after the first turn.
 
 ResidualMoves:
-	db MIST
-	db LEECH_SEED
 	db POISONPOWDER
 	db STUN_SPORE
 	db THUNDER_WAVE
+	db TOXIC
 	db FOCUS_ENERGY
 	db BIDE
+	db GLARE
 	db POISON_GAS
 	db TRANSFORM
 	db CONVERSION
 	db SUBSTITUTE
+	db SPIDER_WEB
+	db BELLY_DRUM
+	db MEAN_LOOK
+	db ATTRACT
 	db SPIKES
 	db -1 ; end
